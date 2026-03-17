ed9ba4b243acd72a8d65edc1ff9e77dcc348812f
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-03-17 20:50:43 +0530

    Create Missing_Number.py

---
+++ Arrays/Missing_Number.py
@@ -0,0 +1,20 @@
+# METHOD 1 :
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        total_sum = n * (n + 1) // 2
+        array_sum = sum(nums)
+        return total_sum - array_sum
+
+
+# METHOD 2
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        for i in range(n + 1):
+            if i not in nums :
+                return i
+
+nums = [3, 0, 1]
+sol = Solution()
+print(sol.missingNumber(nums)) # Output: 2
