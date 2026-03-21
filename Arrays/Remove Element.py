cc4b4da7514d8c2dcfd2b230abe20240f0f8375d
Dhanyatha P L <dhanyathapl129@gmail.com>
2026-03-21 10:00:00 +0530

    updated Remove Element file

---
+++ Arrays/Remove Element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0  # pointer for the position of the next valid element
+        
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+        
+        return k
+        
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+val = 2
+
+obj = Solution()
+k = obj.removeElement(nums, val)
+print(k)      # 5 No. of elements not equal to val
+print(nums)   # [0, 1, 3, 0, 4, 0, 4, 2] -> first 5 elements correct
