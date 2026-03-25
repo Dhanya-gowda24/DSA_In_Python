b192cc983cf3f794f259e2edf8562ce78287e564
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-03-25 22:59:27 +0530

    Create Search insert Position.py

---
+++ Arrays/Search insert Position.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # Initialize search boundaries
+        left, right = 0, len(nums) - 1
+
+        # Standard binary search
+        while left <= right:
+            mid = (left + right) // 2  # Find the middle index
+
+            if nums[mid] == target:
+                # If target is found, return its index
+                return mid
+            elif nums[mid] < target:
+                # If target is greater, ignore left half
+                left = mid + 1
+            else:
+                # If target is smaller, ignore right half
+                right = mid - 1
+
+        return left
+        
+nums = [1, 3, 5, 6]
+target = 2
+
+obj = Solution()
+print(obj.searchInsert(nums, target))
