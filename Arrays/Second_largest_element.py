44e9998e191715a3f38f38869e3123d155ccf4b8
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-03-26 19:52:28 +0530

    Create Second_largest_element.py

---
+++ Arrays/Second_largest_element.py
@@ -0,0 +1,24 @@
+class Solution:
+    def secondLargestElement(self, nums):
+        if len(nums) < 2:
+            print(-1)
+            return
+
+        largest = None
+        second_largest = None
+
+        for x in nums:
+            if largest is None or x > largest:
+                second_largest = largest
+                largest = x
+            elif x != largest and (second_largest is None or x > second_largest):
+                second_largest = x
+
+        if second_largest is None:
+            print(-1)
+        else:
+            print(second_largest)
+        
+nums = [10, 20, 4, 45, 99]
+obj = Solution()
+obj.secondLargestElement(nums) # output: 45
