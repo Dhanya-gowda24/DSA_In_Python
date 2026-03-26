1d6817ef56677e9c05defadcf54cf9f61ec3380a
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-03-26 19:59:16 +0530

    Create Sort_an_array 0's, 1's and 2's.py

---
+++ Arrays/Sort_an_array 0's, 1's and 2's.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortZeroOneTwo(self, nums):
+        low = 0
+        mid = 0 
+        high = len(nums)-1
+        
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] ==1:
+                mid += 1
+            else:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high -= 1
+
+        return nums
+        
+nums = [2,0,2,1,1,0]
+obj = Solution()
+print(obj.sortZeroOneTwo(nums)) # Output : [0, 0, 1, 1, 2, 2]
