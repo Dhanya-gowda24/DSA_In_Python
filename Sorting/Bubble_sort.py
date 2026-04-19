424bd3f984b5583493ec025a5b173338ad0ddeda
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-04-19 23:14:30 +0530

    Add Bubble Sort implementation
    Implement bubble sort algorithm in Python.

---
+++ Sorting/Bubble_sort.py
@@ -0,0 +1,14 @@
+class Solution:
+    def bubbleSort(self, nums):
+        n = len(nums)
+        
+        for i in range(n - 1):
+            for j in range(n - 1 - i):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        print(nums)
+        
+# Example usage
+sol = Solution()
+sol.bubbleSort([64, 34, 25, 12, 22, 11, 90])
