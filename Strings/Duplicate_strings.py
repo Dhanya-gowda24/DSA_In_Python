d9cf3bef9913be21e945003c21ad0c326c82a8a0
Dhanya-gowda24 <dhanyathapl129@gmail.com>
2026-04-27 14:53:28 +0530

    Create Duplicate_strings.py

---
+++ Strings/Duplicate_strings.py
@@ -0,0 +1,17 @@
+def printDuplicates(s):
+    s = ''.join(sorted(s))
+
+    i = 0
+    while i < len(s):
+        count = 1
+        
+        while i + count < len(s) and s[i] == s[i + count]:
+            count += 1
+
+        if count > 1:
+            print(f"['{s[i]}', {count}]")
+
+        i += count
+
+s = "nagarjun p l"
+printDuplicates(s) # Output : [' ', 2] ['a', 2] ['n', 2]
