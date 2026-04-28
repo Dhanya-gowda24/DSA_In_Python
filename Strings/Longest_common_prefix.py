25dfc25b6fed9750da3c7478e2563529b4c7e649
Dhanyatha P L <dhanyathapl129@gmail.com>
2026-04-28 10:00:00 +0530

    updated file

---
+++ Strings/Longest_common_prefix.py
@@ -0,0 +1,15 @@
+def longestCommonPrefix(st):
+    pre = st[0]
+
+    for s in st[1:]:
+        while not s.startswith(pre):
+            pre = pre[:-1]
+            if pre == "":
+                return ""
+
+    return pre
+
+st1 = ["dog", "cat", "animal", "monkey"]
+st2 = ["flowers", "flow", "fly", "flight"]
+print(longestCommonPrefix(st1))
+print(longestCommonPrefix(st2))
